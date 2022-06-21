419ebebfd48cd9fed17d8ef0a5cbb6eb73c15cd9
Aristidis Gianicopulos loguercio <agianico@c3r3s4.42madrid.com>
2022-06-21 19:44:39 +0200

    Headers have been added

--- cpp_01/ex01/main.cpp
+++ cpp_01/ex01/main.cpp
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agianico <agianico@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 19:41:43 by agianico          #+#    #+#             */
+/*   Updated: 2022/06/21 19:41:44 by agianico         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Zombie.hpp"
 
 int main(void)
 {
     std::string name;
     int         N;
-    //Zombie      *zombieHorde;
+    Zombie      *zombieHorde;
 
     std::cout << "Ponle un nombre a tu Zombie:" << std::endl;
     std::getline(std::cin, name);
